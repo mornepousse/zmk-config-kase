1f281b9e753d0284933e5bd12b7337288f5bb9fe
Mae PUGIN <48982737+mornepousse@users.noreply.github.com>
2026-03-22 19:19:47 +0100

    Add shell.nix and ST-Link flash overlay
    shell.nix: nix-shell with west, cmake, gcc-arm-embedded, nrfutil.
    kase_left_stlink.overlay: partition layout without bootloader for
    direct SWD flash via ST-Link (code at 0x0 instead of 0x26000).

---
+++ shell.nix
@@ -0,0 +1,37 @@
+{ pkgs ? import <nixpkgs> {
+    config.allowUnfree = true;
+    config.segger-jlink.acceptLicense = true;
+  } }:
+
+let
+  pythonEnv = pkgs.python313.withPackages (ps: with ps; [
+    west
+    pykwalify
+    ruamel-yaml
+    pyelftools
+    pyyaml
+    packaging
+    intelhex
+    cbor2
+    cryptography
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    pythonEnv
+    pkgs.cmake
+    pkgs.ninja
+    pkgs.dtc
+    pkgs.gcc-arm-embedded
+    pkgs.nrfutil
+  ];
+
+  shellHook = ''
+    export ZEPHYR_BASE=$PWD/zephyr
+    export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
+    export GNUARMEMB_TOOLCHAIN_PATH=${pkgs.gcc-arm-embedded}
+    export PYTHONPATH=${pythonEnv}/lib/python3.13/site-packages
+    echo "KaSe ZMK build environment ready"
+    echo "ZEPHYR_BASE=$ZEPHYR_BASE"
+  '';
+}
